59bbcd43caa28874359acbf4264134499004e15b
Kirti Singh <kirtisingh1497@gmail.com>
2022-09-05 21:22:59 +0530

    Css update

--- app.js
+++ app.js
@@ -1,55 +1,47 @@
-const dob=document.querySelector("#dob");
-const luckyNumber=document.querySelector("#lucky-number");
-const luckybtn=document.querySelector(".luck-num-btn");
-const output=document.querySelector("#output");
+const dob = document.querySelector("#dob");
+const luckyNumber = document.querySelector("#lucky-number");
+const luckybtn = document.querySelector(".luck-num-btn");
+const output = document.querySelector("#output");
 
 console.log(dob);
 console.log(luckyNumber);
-var sum=0;
+// var sum=0;
 
-function clickHandler(){
-
-
-    dobValue=dob.value;
-    luckyValue=luckyNumber.value;
-    var sum=0;
-
-    if(dobValue == "" || luckyValue== "" || luckyValue<=0){
-        output.classList.add("output-err");
-        output.innerText="Please enter valid input 🥸";  
-    }
-    else if(dobValue !="" && luckyValue>0){
-        output.classList.remove("output-err");
-        checkLuckyNumber();
-    }
-
-
+function clickHandler() {
+  dobValue = dob.value;
+  luckyValue = luckyNumber.value;
+  var sum = 0;
+  if (dobValue == "" && (luckyValue == "" || luckyValue <= 0)) {
+    output.classList.add("output-err");
+    output.innerText = "Enter DOB and Lucky Number 🥸";
+  } else if (luckyValue == "" || luckyValue <= 0) {
+    output.classList.add("output-err");
+    output.innerText = "Enter lucky number 🥸";
+  } else if (dobValue == "") {
+    output.classList.add("output-err");
+    output.innerText = "Enter DOB 🥸";
+  } else if (dobValue != "" && luckyValue > 0) {
+    output.classList.remove("output-err");
+    checkLuckyNumber();
+  }
 }
 
+function checkLuckyNumber() {
+  var sum = 0;
+  dobList = dobValue.replaceAll("-", "");
+  console.log("Replaced all", dobList);
+  console.log("adding dob value added", dobValue);
+  for (var i = 0; i < dobList.length; i++) {
+    console.log("in array value of i", dobList[i]);
+    sum += parseInt(dobList[i]);
+    console.log("sum is", sum);
+  }
 
-
-function checkLuckyNumber(){
-    dobList=dobValue.replaceAll('-','');
-    console.log("Replaced all",dobList);
-    console.log("adding dob value added",dobValue);
-    for(var i=0;i<dobList.length;i++){
-        console.log("in array value of i",dobList[i])
-        sum+=parseInt(dobList[i]);
-        console.log("sum is",sum);
-    }
-
-    if(sum%luckyValue == 0){
-        output.innerText="Yeahh!! Your Birthday is LUCKY 🎉🎉";   
-    }else{
-        output.innerText="oops!! Your Birthday is NOT LUCKY 🥴🥴"
-    }
-
+  if (sum % luckyValue == 0) {
+    output.innerText = "Yeahh!! Your Birthday is LUCKY 🎉🎉";
+  } else {
+    output.innerText = "oops!! Your Birthday is NOT LUCKY 🥴🥴";
+  }
 }
 
-
-
-
-
-
-
-luckybtn.addEventListener('click',clickHandler);
+luckybtn.addEventListener("click", clickHandler);
